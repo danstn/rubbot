b70c8537712de832a552660f98d0cdffd39f9171
Daniel Stankevich <standeo@gmail.com>
2014-09-14 20:14:22 +1000

    Rubbot#start simple coverage

--- spec/lib/rubbot_spec.rb
+++ spec/lib/rubbot_spec.rb
@@ -10,4 +10,17 @@ describe Rubbot do
       expect(rubbot).to be_a Rubbot
     end
   end
+
+  describe "#start" do
+    let(:stdin) { Utils::StdinSimulator.new }
+    let(:stdout) { StringIO.new }
+
+    context "invalid command supplied" do
+      it "rescue an exception" do
+        stdin << "rescue me"
+        rubbot = Rubbot.new(grid, stdin: stdin, stdout: stdout)
+        expect { rubbot.start }.to_not raise_error
+      end
+    end
+  end
 end
